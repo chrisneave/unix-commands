20fe3b6947e033e08cd400c79cf0c551e0efc3b4
Chris Neave <chrispneave@gmail.com>
2015-04-09 21:09:08 +0100

    Use writeTail to output last n lines of a file
    Experiment with a new way of following files
    
    Seeking should not be required as we are keeping file handles open
    and file position will be retained.

--- tail/tail.go
+++ tail/tail.go
@@ -26,8 +26,9 @@ type tailedFile struct {
 func main() {
 	flag.Parse()
 	tailedFiles := make([]tailedFile, flag.NArg())
+	writer := bufio.NewWriter(os.Stdout)
 	var err error
-	var header string
+	var fi os.FileInfo
 
 	for i, arg := range flag.Args() {
 		tailedFiles[i] = tailedFile{filename: arg}
@@ -38,54 +39,45 @@ func main() {
 		}
 		defer tailedFiles[i].file.Close()
 
+		fi, err = tailedFiles[i].file.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		tailedFiles[i].lastFileSize = fi.Size()
+
 		if len(tailedFiles) > 1 {
-			header = fmt.Sprintf("==> %s <==", tailedFiles[i].filename)
+			tailedFiles[i].writeHeaderTo(writer)
 		}
 
-		tailedFiles[i].offset = seekAndOutput(tailedFiles[i].file, tailedFiles[i].offset, header)
+		tailedFiles[i].offset = writeTail(tailedFiles[i].file, writer, *limit)
 	}
 
 	for {
-		for _, tf := range tailedFiles {
-			fs, err := os.Stat(tf.filename)
-			if err != nil {
-				log.Fatal(err)
+		for i := 0; i < len(tailedFiles); i++ {
+			if tailedFiles[i].hasChanged() == false {
+				continue
 			}
 
-			tf.follow(fs.Size(), len(tailedFiles) > 1)
+			tailedFiles[i].lastFileSize = fi.Size()
+			if len(tailedFiles) > 1 {
+				tailedFiles[i].writeHeaderTo(writer)
+			}
+
+			reader := bufio.NewReader(tailedFiles[i].file)
+			for {
+				line, err := reader.ReadString(10)
+				if len(line) > 0 {
+					writer.WriteString(line)
+				}
+				if err != nil {
+					writer.Flush()
+					break
+				}
+			}
 		}
 
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(1000 * time.Millisecond)
 	}
-}
-
-func (tf *tailedFile) follow(currentSize int64, outputHeader bool) {
-	if tf.lastFileSize < currentSize {
-		tf.offset = seekAndOutput(tf.file, tf.offset, fmt.Sprintf("==> %s <==", tf.filename))
-		tf.lastFileSize = currentSize
-	}
-}
-
-func seekAndOutput(source io.ReadSeeker, offset int64, header string) (newOffset int64) {
-	var currentLines []string
-
-	offset, err := source.Seek(offset, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	reader := bufio.NewReader(source)
-	currentLines, newOffset = tailScan(reader, *limit, offset)
-
-	if len(header) > 0 && len(currentLines) > 0 {
-		fmt.Println(header)
-	}
-
-	for _, line := range currentLines {
-		fmt.Println(line)
-	}
-
-	return
 }
 
 func appendAndTail(dst []string, src []string, limit int) []string {
@@ -127,4 +119,22 @@ func writeTail(input io.Reader, output io.Writer, lineCount int) (newOffset int6
 	}
 	writer.Flush()
 	return
+}
+
+func (tf *tailedFile) writeHeaderTo(writer *bufio.Writer) {
+	writer.WriteString(fmt.Sprintf("==> %s <==", tf.filename))
+	writer.WriteString("\n")
+}
+
+func (tf *tailedFile) hasChanged() bool {
+	fi, err := tf.file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if fi.Size() == tf.lastFileSize {
+		return false
+	}
+
+	return true
 }
