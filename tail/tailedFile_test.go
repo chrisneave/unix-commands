4bf14e358b0b3bdee38d61e37cdbb2d2ef6760d3
Chris Neave <chrispneave@gmail.com>
2015-04-11 23:02:40 +0100

    Tests for tailedFile.hasChanged()

--- tail/tailedFile_test.go
+++ tail/tailedFile_test.go
@@ -40,3 +40,26 @@ func TestTailedFileWriteHeaderTo(t *testing.T) {
 		t.Errorf("Expected %s but got %s", expected, output.String())
 	}
 }
+
+func TestTailedFileHasNotChanged(t *testing.T) {
+	ff := &FakeFile{name: "foo.txt"}
+	tf := createSubject(ff)
+
+	actual := tf.hasChanged()
+
+	if actual != false {
+		t.Error("File has changed when it should not")
+	}
+}
+
+func TestTailedFileHasChanged(t *testing.T) {
+	ff := &FakeFile{name: "foo.txt", size: 100}
+	tf := createSubject(ff)
+	tf.lastFileSize = 50
+
+	actual := tf.hasChanged()
+
+	if actual != true {
+		t.Error("File has not changed when it should have")
+	}
+}
